4826d3ddfdc8ab70038f1755c043e4325c802126
Grayson Bailey <grayson.d.bailey@gmail.com>
2020-04-15 16:25:54 +0200

    copy // master

---
+++ public/tecturalDisc.js
@@ -0,0 +1,80 @@
+var socket;
+var tFont;
+
+function preload() {
+  tFont = loadFont("OCRAStd.otf");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(51);
+  textFont(tFont);
+  socket = io.connect('localhost:3000');
+  socket.on('mouse', newDrawing);
+}
+
+function draw() {
+  background(0);
+  fill(0);
+
+  let hello = d => console.log(d)
+
+
+  curs();
+
+}
+
+
+
+
+function curs() {
+  stroke(47,230,240);
+  push();
+  translate(mouseX,mouseY);
+  strokeWeight(3);
+  point(0, 5);
+  point(5, 0);
+  point(0, 0);
+  pop();
+  noStroke();
+}
+
+
+function newDrawing(data, tex) {
+  noStroke();
+  fill(255, 0, 100);
+  console.log(data.talk);
+
+  ellipse(data.x, data.y, 36, 36);
+
+  fill(0);
+  text(data.talk, data.x, data.y);
+
+}
+
+
+function mouseDragged() {
+
+  var tex = "loser";
+
+  var data = {
+    x: mouseX,
+    y: mouseY,
+    talk: tex
+  }
+
+  console.log(data.talk);
+
+  socket.emit('mouse', data);
+  socket.emit('text', tex);
+
+  noStroke();
+  fill(255);
+  ellipse(mouseX, mouseY, 60, 60);
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(51);
+}
---
+++ server.js
@@ -0,0 +1,39 @@
+
+
+
+var express = require('express');
+var app = express();
+
+
+app.use(express.static('public'));
+
+var server = app.listen(3000,'0.0.0.0');
+
+console.log("My socket server is running");
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.on('connection', newConnection);
+
+function newConnection(socket) {
+  console.log('new connection: ' + socket.id);
+
+  socket.on('mouse',mouseMsg);
+  //socket.on('text', textTalk);
+
+//  function textPlace(tex){
+//console.log(tex);
+
+
+  //}
+
+  function mouseMsg(data){
+    socket.broadcast.emit('mouse',data);
+    console.log(data);
+    console.log(data.talk);
+  //  console.log(tex);
+  }
+
+
+}
