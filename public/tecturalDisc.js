cfa2ddcc0eaa766f60def97db9dee0700af209f5
Grayson Bailey <grayson.d.bailey@gmail.com>
2020-04-17 22:01:33 +0200

    summary updates
    changed folder positions,
    worked with aspects in css
    and html files
    
    Got the scroll to work again and everything

--- public/tecturalDisc.js
+++ public/tecturalDisc.js
@@ -1,61 +1,71 @@
-var socket;
-var tFont;
+let socket;
+let tFont;
 let curs;
-var discourse;
-var pointers = [];
+let discourse;
+let pointers = [];
+let position = 0
 
 
 window.onload = function() {
   document.getElementById('about-this-website').onclick = () => {
     document.getElementById('about-window-overlay').classList.remove('disabled');
+    console.log("pressed it")
   }
 }
 
 
 
 function preload() {
-  tFont = loadFont("OCRAStd.otf");
+  tFont = loadFont("assets/OCRAStd.otf");
   discourse = loadJSON("allgemeine.json");
 }
 
 
-function displayDiscourse() {
-  fill(255);
-  text(discourse, 100, 100, 400, 1000)
-  var units = discourse.units
 
-  for (var each in units) {
+function displayDiscourse() {
+  fill(255,0,0);
+  text(discourse, 100, 100, 400, 1000)
+  let units = discourse.units
+
+  for (let each in units) {
     let unit = units[each];
-    text(unit.Base, unit.PosX, unit.PosY, 400, 1000)
-    console.log(units[each])
+    text(unit.Base, unit.PosX, unit.PosY+position, 400, 1000)
+    //console.log(units[each])
   }
 }
 
 
 
-// for( var element of discourse){
-// console.log(element.PosX)
-//
-// }
-function setStage() {
-  createCanvas(windowWidth, windowHeight);
+
+function refresh() {
   background(0);
-  textFont(tFont);
-  socket = io.connect('localhost:3000');
-  socket.on('mouse', newDrawing);
-  cursor("swift.png")
+
+
+  cursor("assets/swift.png")
   displayDiscourse();
   pointers = [createVector(0, 0), createVector(0, 0)]
+
+  translate(0,position);
 }
 
 
 
 function setup() {
-  setStage();
+  createCanvas(windowWidth, windowHeight);
+  textFont(tFont);
+  socket = io.connect('localhost:3000');
+  socket.on('mouse', newDrawing);
+  refresh();
+
 }
 
-// function draw() {
-// }
+function draw(){
+
+
+
+
+
+}
 
 
 function setPositions() {
@@ -89,7 +99,7 @@ function mouseDragged() {
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   background(0);
-  setStage();
+  refresh();
 }
 
 function mousePressed() {
@@ -117,4 +127,10 @@ function mouseClicked() {
 
 function mouseMoved() {
   setPositions()
+}
+
+function mouseWheel(event){
+console.log(event.delta);
+position-= event.delta;
+refresh();
 }
