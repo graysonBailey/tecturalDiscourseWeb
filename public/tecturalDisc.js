b96ca6103fab4141d846f310649675d5e82c44a7
Grayson Bailey <grayson.d.bailey@gmail.com>
2020-04-17 16:17:28 +0200

    rudimentary point and click line
    test line creation, but no relation / meaning behind it. Will need to add relations json into combined json file...

--- public/tecturalDisc.js
+++ public/tecturalDisc.js
@@ -2,15 +2,13 @@ var socket;
 var tFont;
 let curs;
 var discourse;
+var pointers = [];
 
 
 window.onload = function() {
   document.getElementById('about-this-website').onclick = () => {
     document.getElementById('about-window-overlay').classList.remove('disabled');
   }
-
-
-
 }
 
 
@@ -18,24 +16,19 @@ window.onload = function() {
 function preload() {
   tFont = loadFont("OCRAStd.otf");
   discourse = loadJSON("allgemeine.json");
-
 }
 
 
-function displayDiscourse(){
+function displayDiscourse() {
   fill(255);
-  text(discourse,100,100,400,1000)
-  var units =discourse.units
+  text(discourse, 100, 100, 400, 1000)
+  var units = discourse.units
 
-for(var each in units){
-
-  let unit = units[each];
-
-  text(unit.Base,unit.PosX,unit.PosY,400,1000)
-console.log(units[each])
-}
-
-
+  for (var each in units) {
+    let unit = units[each];
+    text(unit.Base, unit.PosX, unit.PosY, 400, 1000)
+    console.log(units[each])
+  }
 }
 
 
@@ -44,8 +37,7 @@ console.log(units[each])
 // console.log(element.PosX)
 //
 // }
-function setStage(){
-
+function setStage() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   textFont(tFont);
@@ -53,19 +45,17 @@ function setStage(){
   socket.on('mouse', newDrawing);
   cursor("swift.png")
   displayDiscourse();
-
+  pointers = [createVector(0, 0), createVector(0, 0)]
 }
 
 
 
 function setup() {
-setStage();
+  setStage();
 }
 
-function draw() {
-
-
-}
+// function draw() {
+// }
 
 
 function setPositions() {
@@ -73,46 +63,33 @@ function setPositions() {
   document.getElementById("y-coord").innerHTML = mouseY
 }
 
-
-
-
-
-
-
-
 function newDrawing(data, tex) {
   noStroke();
   fill(255, 0, 100);
-  fill(230,47,240);
+  fill(230, 47, 240);
   text(data.talk, data.x, data.y);
-
 }
 
 
 function mouseDragged() {
 
   var tex = "loser";
-
   var data = {
     x: mouseX,
     y: mouseY,
     talk: tex
   }
-
-  console.log(data.talk);
-
   socket.emit('mouse', data);
   socket.emit('text', tex);
-
   noStroke();
-  fill(47,230,240);
+  fill(47, 230, 240)
   ellipse(mouseX, mouseY, 20, 20);
-
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   background(0);
+  setStage();
 }
 
 function mousePressed() {
@@ -120,10 +97,22 @@ function mousePressed() {
     let fs = fullscreen();
     fullscreen(!fs);
   }
+}
 
-
-
-
+function mouseClicked() {
+  if (pointers[0].x + pointers[0].y == 0) {
+    pointers[0].x = mouseX
+    pointers[0].y = mouseY
+    console.log("POINTER ONE" + pointers[0])
+  } else {
+    pointers[1].x = mouseX
+    pointers[1].y = mouseY
+    console.log("POINTER TWO" + pointers[1])
+    stroke('#ffA908');
+    strokeWeight(1);
+    line(pointers[0].x, pointers[0].y, pointers[1].x, pointers[1].y);
+    pointers[0] = createVector(0, 0);
+  }
 }
 
 function mouseMoved() {
