97d55472e33f675cf26a83924ff24955f61698a4
Grayson Bailey <grayson.d.bailey@gmail.com>
2020-04-21 19:39:17 +0200

    //

--- src/index.js
+++ src/index.js
@@ -9,39 +9,39 @@ import p5 from 'p5';
 
 // const containerElement = document.getElementById('p5-container');
 
-const sketch = (p) => {
-  let x = 100;
-  let y = 100;
-  let can;
-
-  p.setup = function() {
-    can = p.createCanvas(800, 400);
-      p.background(0);
-    can.mousePressed(p.drawEllipse);
-  };
-
-  p.draw = function() {
-    p.fill(255);
-
-
-
-
-  };
-
-  p.drawEllipse = function(){
-
-    p.ellipse(p.mouseX,p.mouseY,100,100);
-    console.log("worked")
-
-
-  }
+// const sketch = (p) => {
+//   let x = 100;
+//   let y = 100;
+//   let can;
+//
+//   p.setup = function() {
+//     can = p.createCanvas(800, 400);
+//       p.background(0);
+//     can.mousePressed(p.drawEllipse);
+//   };
+//
+//   p.draw = function() {
+//     p.fill(255);
+//
+//
+//
+//
+//   };
+//
+//   p.drawEllipse = function(){
+//
+//     p.ellipse(p.mouseX,p.mouseY,100,100);
+//     console.log("worked")
+//
+//
+//   }
 
 //   function p.mouseClicked(){
 //     p.fill(255);
 // p.ellipse(mouseX,mouseY,100,100);
 //
 //   }
-};
+// };
 //
 new p5(sketch);
 
