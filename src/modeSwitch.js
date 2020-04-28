a164253183edbd9d28d08f9ebb327aecd2eb4ec6
Grayson Bailey <grayson.d.bailey@gmail.com>
2020-04-28 19:00:06 +0200

    quick update after reversion

--- src/modeSwitch.js
+++ src/modeSwitch.js
@@ -1,5 +1,5 @@
 export default function switchModeInstructions(mode) {
-  let geistInstructions = "Geist Instruction\r\n \r\n...use SPACE to open an input space on the canvas\r\n \r\n...type to provide a discursive unit of text: reference of response\r\n \r\n...use ENTER to emit the input into the discourse\r\n \r\n...or use ESCAPE to remove yourself from the situation"
+  let geistInstructions = "Geist Instruction\r\n \r\n...use SPACE to open an input space on the canvas\r\n \r\n...type to provide a discursive unit of text: reference or response\r\n \r\n...use ENTER to emit the input into the discourse\r\n \r\n...or use ESCAPE to remove yourself from the situation"
   let relationsInstructions = "currently under construction\r\n \r\ncome back in a few days, weeks, years..."
   let element = document.getElementById('instructions')
   switch (mode) {
